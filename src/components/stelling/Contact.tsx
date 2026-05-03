ac916b0cef74cd5ac1c2e3e2bed79bdfef320bda
cyber21330 <21330@proton.me>
2026-05-03 04:26:21 +0200

    cambio en el texto pues escribirnos a

--- src/components/stelling/Contact.tsx
+++ src/components/stelling/Contact.tsx
@@ -219,7 +219,7 @@ emailjs.send(
 
             <p className="font-mono-ui text-center mt-2" style={{ color: "#78768A", fontSize: 11 }}>
               También puedes escribirnos a{" "}
-              <a href="mailto:hola@stellingsecure.es" style={{ color: "#BF9339" }}>
+              <a href="mailto:hola@stellingsecure.com" style={{ color: "#BF9339" }}>
                 hola@stellingsecure.es
               </a>
             </p>
